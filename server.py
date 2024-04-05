be4bcf2e92ae171396901cbb067b113a600c74b3
karatasyusuf97 <yusuf.karatas231997@gmail.com>
2024-04-05 13:22:00 +0300

    dasd

--- server.py
+++ server.py
@@ -84,4 +84,32 @@ def delete_by_id(id):
             return {"message":f"{id} removed"}, 200
     return {"message": f"{id} not found"}, 404
 
-@app
+
+
+@app.route("/calculator")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/calculator/sum")
+def sum_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = summation(num1,num2)
+    return str(result)
+
+@app.route("/calculator/sub")
+def sub_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = subtraction(num1,num2)
+    return str(result)
+
+@app.route("/calculator/mul")
+def mul_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = multiplication(num1,num2)
+    return str(result)
+
+    
+app.run(debug=1)  
