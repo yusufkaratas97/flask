fe969e1cf1980e17a67c9789ffdf1e5a82eaf488
karatasyusuf97 <yusuf.karatas231997@gmail.com>
2024-04-05 13:12:07 +0300

    adding calculator

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # flask
 different web operations on Flask by Python
+
+-server.py: working on data given in the code.
+    to run it:
+        first terminal: flask --app server --debug run
+        second terminal: curl -X GET -i '\n' localhost:5000\{CHOoSE_the_app}
+
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, render_template
+from mathematics import summation, subtraction, multiplication
 
 app = Flask(__name__)
 
@@ -82,3 +83,33 @@ def delete_by_id(id):
             data.remove(person)
             return {"message":f"{id} removed"}, 200
     return {"message": f"{id} not found"}, 404
+
+
+
+@app.route("/calculator")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/calculator/sum")
+def sum_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = summation(num1,num2)
+    return str(result)
+
+@app.route("/calculator/sub")
+def sub_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = subtraction(num1,num2)
+    return str(result)
+
+@app.route("/calculator/mul")
+def mul_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = multiplication(num1,num2)
+    return str(result)
+
+    
+app.run(debug=1)  
